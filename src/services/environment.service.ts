8ffab1e2a7e9c8aa3765b727ad6211de3542e161
Luis Fonseca <01luisfonseca@gmail.com>
2024-10-19 20:41:37 -0500

    Add environment

--- src/services/environment.service.ts
+++ src/services/environment.service.ts
@@ -1,14 +1,17 @@
 import { DeviceReader } from '../entities/deviceReader.entity'
+import { Repository } from '../entities/repository.entity'
 
-export class EnvironmentService {
+export class EnvironmentService<TFields, T> {
   private deviceReader: DeviceReader
+  private repository: Repository<TFields, T>
 
-  constructor(deviceReader: DeviceReader) {
+  constructor(deviceReader: DeviceReader, repository: Repository<TFields, T>) {
     this.deviceReader = deviceReader
+    this.repository = repository
   }
 
-  async getTemperature(): Promise<number> {
-    const data = await this.deviceReader.read()
-    return data.temperature
+  async saveMeasure(): Promise<void> {
+    const data = (await this.deviceReader.read()) as TFields
+    await this.repository.create(data)
   }
 }
